6a1f0f453ef38d214dcedf99f7eda54003a36e22
ivanprytula <ivanprytula87@gmail.com>
2022-05-04 18:31:46 +0300

    add word/token selection, speed boost logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,48 @@
 // Walking snake behavior
 
-const STEP_SIZE = 10;
-
-//init object globally
-let objImage = null;
+const walkingSnake = document.querySelector("#walkingSnake");
+let stepSize;
 
 
-const init = () => {
-  //getting the image node
-  objImage = document.querySelector("#walkingSnake");
-  objImage.style.left = "0px";
-  objImage.style.top = "0px";
-};
-
+walkingSnake.style.left = "0px";
+walkingSnake.style.top = "0px";
 
 const moveUp = () => {
-  objImage.style.top = parseInt(objImage.style.top) - STEP_SIZE + "px";
-  objImage.className = ""
-  objImage.className = "walking-snake ld ld-float"
+  walkingSnake.style.top = parseInt(walkingSnake.style.top) - stepSize + "px";
+  walkingSnake.className = ""
+  walkingSnake.className = "walking-snake ld ld-float"
 };
 
 const moveDown = () => {
-  objImage.style.top = parseInt(objImage.style.top) + STEP_SIZE + "px";
-  objImage.className = ""
-  objImage.className = "walking-snake ld ld-pulse"
+  walkingSnake.style.top = parseInt(walkingSnake.style.top) + stepSize + "px";
+  walkingSnake.className = ""
+  walkingSnake.className = "walking-snake ld ld-pulse"
 };
 
 const moveLeft = () => {
-  objImage.style.left = parseInt(objImage.style.left) - STEP_SIZE + "px";
-  objImage.className = ""
-  objImage.className = "walking-snake ld ld-jelly"
+  walkingSnake.style.left = parseInt(walkingSnake.style.left) - stepSize + "px";
+  walkingSnake.className = ""
+  walkingSnake.className = "walking-snake ld ld-jelly"
 };
 
 const moveRight = () => {
-  objImage.style.left = parseInt(objImage.style.left) + STEP_SIZE + "px";
-  objImage.className = ""
-  objImage.className = "walking-snake ld ld-jingle"
+  walkingSnake.style.left = parseInt(walkingSnake.style.left) + stepSize + "px";
+  walkingSnake.className = ""
+  walkingSnake.className = "walking-snake ld ld-jingle"
 };
+
+const inspectToken = (token) => {
+  console.log('token:: >> ', token);
+}
 
 
 document.body.onkeydown = (e) => {
+  if (e.repeat) {
+    stepSize++;
+  } else {
+    stepSize = 10;
+  }
+
   switch (e.key) {
     case 'ArrowLeft':
       moveLeft();
@@ -56,4 +59,14 @@ document.body.onkeydown = (e) => {
   }
 }
 
-window.onload = init;
+document.body.addEventListener('click', (e) => {
+  if (walkingSnake.contains(e.target)) {
+    console.log('clicked inside');
+  } else {
+    let selection = window.getSelection();
+    selection.modify('move', 'backward', 'word');
+    selection.modify('extend', 'forward', 'word');
+
+    inspectToken(selection.toString());
+  }
+});
